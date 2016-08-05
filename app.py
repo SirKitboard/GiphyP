dd8463562bf22534e64e941483622934cd846a90
Aditya Balwani <adibalwani@gmail.com>
2016-08-05 15:14:00 -0400

    Another commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def hello():
     for key in request.form:
         print(key + " : " + str(request.form[key]))
+
+    return "HELLO"
 
 if __name__ == "__main__":
     app.run()

15c8bf1f711cf8655821fad696edb3dc6fe0cb95
root <adibalwani@gmail.com>
2016-08-05 20:51:54 +0000

    Fixes

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 import requests
 from os.path import join, dirname
-from dotenv import load_dotenv
 from flask.ext.api import status
 import json
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+import os
+from dotenv import Dotenv
+dotenv = Dotenv(os.path.join(os.path.dirname(__file__), ".env")) # Of course, replace by your correct path
+os.environ.update(dotenv)
 
 SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
 
@@ -28,18 +29,21 @@ def hello():
             "offset": 0,
         }
 
-    if(user_state_dict[username][state] == 0):
+    if(user_state_dict[username]["state"] == 0):
         results = returnSearch(0, request.form["text"], username)
         responseDict = {"attachments": []}
         for link in results:
-            responseDict["attachments"].append({"image_url": link})
+            responseDict["attachments"].append({"image_url": link, "text": "Gif"})
+        responseDict["text"] = "Response"
 
         return jsonify(**responseDict)
 
-    elif(user_state_dict[username][state] == 1):
+    elif(user_state_dict[username]["state"] == 1):
         if(isInt(request.form["text"]) and int(request.form["text"]) < 4 and int(request.form["text"]) > 0):
-            responseDict = {"attachments": [{"image_url":user_state_dict[username]["last_results"][int(request.form["text"])]}]}
+            responseDict = {"attachments": [{"text":" ", "image_url":user_state_dict[username]["last_results"][int(request.form["text"])]}]}
             responseDict["response_type"] = "in_channel"
+            responseDict["text"] = "@"+username+": "+ user_state_dict[username]["last_query"]
+            print responseDict
             return jsonify(**responseDict)
 
     return status.HTTP_404_NOT_FOUND
@@ -52,7 +56,7 @@ def isInt(value):
     return False
 
 def returnSearch(offset, query, username):
-    params = {
+    payload = {
         "limit":3,
         "offset": offset,
         "q": query,
@@ -61,16 +65,20 @@ def returnSearch(offset, query, username):
 
     r = requests.get("http://api.giphy.com/v1/gifs/search", params=payload)
     links = []
-    for data in json.loads(r.text)["data"]:
-        links.append[data["url"]]
+    data = r.json()
+    #data["data"]
+    for giphy in data["data"]:
+        print(giphy)
+       # dadict = json.loads(giphy)
+        links.append(giphy['images']['fixed_height']['url'])
 
     user_state_dict[username]["state"] = 1
     user_state_dict[username]["last_results"] = links
     user_state_dict[username]["last_query"] = request.form["text"]
-    user_state_dict[offset]["offset"] = offset + 3
+    user_state_dict[username]["offset"] = offset + 3
 
     return links
 
 
 if __name__ == "__main__":
-    app.run(threaded=True)
+    app.run()
